aae9f52ac347957aa306b63bff1e61128df4b1ed
jack-m1998 <2654442182@qq.com>
2024-05-25 01:00:01 +0800

    提交print用法测试文件

---
+++ print_use.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Author: majia
+time: 2024/5/25
+filename: print_use
+"""
+
+
+def print_sample():
+    # 单引号输出，一对单引号，单引号里可以用双引号，并可以将单引号打印出来
+    print('hello')
+    print('"hello"')
+
+
+if __name__ == '__main__':
+    print_sample()
