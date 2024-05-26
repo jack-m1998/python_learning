2fb7c9181ea227532bc9afe68001c49148a633bb
jack-m1998 <2654442182@qq.com>
2024-05-26 15:41:13 +0800

    更新pandas测试程序

--- pandas_test.py
+++ pandas_test.py
@@ -9,12 +9,37 @@ import numpy as np
 import pandas as pd
 
 
-def readExcel():
-    df = pd.DataFrame(pd.read_excel(r'C:\Users\26544\Desktop\测试1\4月订单明细表.xlsx'))
+def excel_test():
+    source_data = pd.read_excel(r'C:\Users\26544\Desktop\测试1\4月订单明细表.xlsx')
+    df = pd.DataFrame(source_data)
     print(df)
+
     # 维度查看
-    print(df.shape)
+    # print("维度：\n{}".format(df.shape))
+
+    # 数据表信息
+    # print("数据表信息：\n{}".format(df.info))
+
+    # 查看所有行的列索引名
+    # print("得到一个对象：\n{}".format(df.index))
+    # print("得到一个列表：\n{}".format(df.index.values))
+
+    # 查看所有列的列索引名
+    # print("得到一个对象：\n{}".format(df.columns))
+    # print("得到一个列表：\n{}".format(df.columns.values))
+
+    # 查看某一列的所有值
+    # print("查看某一列的所有值：\n{}".format(df["买家帐号"].values))
+
+    # 查看第3行的数据
+    # print("查看某一列的所有值：\n{}".format(df.iloc[3].values))
+
+    # 查看数据表的值
+    # print("查看数据表的值：\n{}".format(df.values))
+
+    # 数据输出
+    source_data.to_excel(r'C:\Users\26544\Desktop\测试1\test.xlsx', sheet_name='4月订单明细表', index=False)
 
 
 if __name__ == '__main__':
-    readExcel()
+    excel_test()
