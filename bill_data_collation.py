b87d42305143207bc149dbdee53982259986898b
jack-m1998 <2654442182@qq.com>
2024-06-02 22:48:34 +0800

    增加处理带‘*’符号的用户分类处理

--- bill_data_collation.py
+++ bill_data_collation.py
@@ -21,7 +21,18 @@ def clean_filename(filename):
     return clean_name
 
 
-def data_classification(input_excel_name, output_dir, column_name):
+def save_groups(grouped, sheet_name, output_dir):
+    for group_name, group in tqdm(grouped, desc='分类文件正在保存', unit='file'):
+        output_path = os.path.join(output_dir, clean_filename(f"{group_name}.xlsx"))
+        if not os.path.exists(output_path):
+            with ExcelWriter(output_path, engine='openpyxl') as writer:
+                group.to_excel(writer, sheet_name=sheet_name, index=False)
+        else:
+            with ExcelWriter(output_path, engine='openpyxl', mode='a', if_sheet_exists='new') as writer:
+                group.to_excel(writer, sheet_name=sheet_name, index=False)
+
+
+def data_classification(input_excel_name, output_dir, first_column_name, second_column_name):
     base_name = os.path.basename(input_excel_name)
     print(f"================= 开始分类：{base_name} ==============")
 
@@ -43,39 +54,65 @@ def data_classification(input_excel_name, output_dir, column_name):
         return
 
         # 尝试自动查找包含"买家账号"的列名
-    try:
-        buyer_account_column = df.columns[df.columns.str.contains(column_name)][0]
-    except IndexError:
-        print(f"错误：找不到包含'{column_name}'的列。")
+    # try:
+    #     buyer_account_column = df.columns[df.columns.str.contains(first_column_name)][0]
+    # except IndexError:
+    #     print(f"错误：找不到包含'{first_column_name}'的列。")
+    #     return
+
+    # 检查买家账号列是否存在
+    if first_column_name not in df.columns:
+        print(f"错误：找不到'{first_column_name}'列。")
         return
 
     # 检查输出目录是否存在，如果不存在则创建
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    # 数据分类（“买家账号”带有“*”号的根据“店铺”列进行分类，不带有“*”号的则正常根据“买家账号”列进行分类）
+    star_mask = df[first_column_name].str.contains(r'\*', regex=True)
+    normal_accounts = df[~star_mask]
+    star_accounts = df[star_mask]
+
+    # 输出工作簿名称
+    output_sheet_name = os.path.splitext(os.path.basename(input_excel_name))[0]
+
+    # 处理不带"*"的买家账号,根据‘买家账号分类’
+    normal_grouped = normal_accounts.groupby(first_column_name)
+    save_groups(normal_grouped, output_sheet_name, output_dir)
+
+    # 处理带"*"的买家账号,根据‘店铺’列分类
+    if not star_accounts.empty:
+        if second_column_name not in df.columns:
+            print(f"错误：找不到'{second_column_name}'列。")
+            return
+        else:
+            star_grouped = star_accounts.groupby(second_column_name)
+            save_groups(star_grouped, output_sheet_name, output_dir)
+
     # 按买家账号分组
-    grouped = df.groupby(buyer_account_column)
-    input_filename = os.path.splitext(os.path.basename(input_excel_name))[0]
+    # grouped = df.groupby(buyer_account_column)
+    # input_filename = os.path.splitext(os.path.basename(input_excel_name))[0]
 
     # 遍历每个分组，并将其保存为单独的Excel文件，分类的文件工作簿名为输入excel文件名
-    for buyer_account, group in tqdm(grouped, desc='分类文件正在保存', unit='file'):
-        # 创建输出文件的路径
-        output_path = os.path.join(output_dir, clean_filename(f"{buyer_account}.xlsx"))
-        # try:
-        #     # 保存分组到Excel文件
-        #     group.to_excel(output_path, sheet_name=input_filename, index=False)
-        # except Exception as e:
-        #     print(f"保存文件时发生错误：{e}")
-        #     continue
-
-        # 检查文件是否存在，不存在则创建，存在则追加
-        if not os.path.exists(output_path):
-            with ExcelWriter(output_path, engine='openpyxl') as writer:
-                group.to_excel(writer, sheet_name=input_filename, index=False)
-        else:
-            # 使用ExcelWriter的mode='a'参数来追加数据
-            with ExcelWriter(output_path, engine='openpyxl', mode='a', if_sheet_exists='new') as writer:
-                group.to_excel(writer, sheet_name=input_filename, index=False)
+    # for buyer_account, group in tqdm(grouped, desc='分类文件正在保存', unit='file'):
+    #     # 创建输出文件的路径
+    #     output_path = os.path.join(output_dir, clean_filename(f"{buyer_account}.xlsx"))
+    #     # try:
+    #     #     # 保存分组到Excel文件
+    #     #     group.to_excel(output_path, sheet_name=input_filename, index=False)
+    #     # except Exception as e:
+    #     #     print(f"保存文件时发生错误：{e}")
+    #     #     continue
+    #
+    #     # 检查文件是否存在，不存在则创建，存在则追加
+    #     if not os.path.exists(output_path):
+    #         with ExcelWriter(output_path, engine='openpyxl') as writer:
+    #             group.to_excel(writer, sheet_name=input_filename, index=False)
+    #     else:
+    #         # 使用ExcelWriter的mode='a'参数来追加数据
+    #         with ExcelWriter(output_path, engine='openpyxl', mode='a', if_sheet_exists='new') as writer:
+    #             group.to_excel(writer, sheet_name=input_filename, index=False)
 
     print(f"=================分类完成：{base_name}=================\n")
 
@@ -127,14 +164,19 @@ if __name__ == '__main__':
     # outputDir = r'测试1\output'
     # columnName = '买家帐号'
 
+    # 参数描述：
+    #   参数1：输入文件路径
+    #   参数2：输出文件路劲
+    #   参数3：一级分类列
+    #   参数4：二级分类列（当一列分类列中包含'*'符号时，根据二级分类列分类）
     excel_files = [
-        (r'测试1\4月订单明细表.xlsx', r'测试1\分类结果', '买家帐号'),
-        (r'测试1\余额变动明细.xlsx', r'测试1\分类结果', '客户'),
-        (r'测试1\销售退货单.xlsx', r'测试1\分类结果', '买家帐号'),
-        (r'测试1\销售出货单.xlsx', r'测试1\分类结果', '买家账号'),
+        (r'测试1\4月订单明细表.xlsx', r'测试1\分类结果', '买家帐号', '店铺'),
+        (r'测试1\余额变动明细.xlsx', r'测试1\分类结果', '客户', '店铺'),
+        (r'测试1\销售退货单.xlsx', r'测试1\分类结果', '买家帐号', '店铺'),
+        (r'测试1\销售出货单.xlsx', r'测试1\分类结果', '买家账号', '店铺'),
     ]
 
-    data_handle(r'测试1\4月订单明细表_handle.xlsx', r'测试1\店铺折扣.xlsx')
+    # data_handle(r'测试1\4月订单明细表_handle.xlsx', r'测试1\店铺折扣.xlsx')
 
-    # for excelName, outputDir, columnName in excel_files:
-    #     data_classification(excelName, outputDir, columnName)
+    for excelName, outputDir, firstColumnName, SecondColumnName in excel_files:
+        data_classification(excelName, outputDir, firstColumnName, SecondColumnName)
